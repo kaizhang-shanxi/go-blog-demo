0a2b3adc90371bfe872da294b07762ec4ee61262
kaizhang <kaizhang.shanxi@gmail.com>
2016-07-18 10:24:03 +0800

    test hostname

--- blog/local.go
+++ blog/local.go
@@ -17,8 +17,8 @@ import (
 )
 
 var (
-	httpAddr     = flag.String("http", ":8080", "HTTP listen address")
-	// httpAddr     = flag.String("http", "localhost:8080", "HTTP listen address")
+	// httpAddr     = flag.String("http", ":8080", "HTTP listen address")
+	httpAddr     = flag.String("http", "localhost:8080", "HTTP listen address")
 	// httpAddr     = flag.String("http", "0.0.0.0:8080", "HTTP listen address")
 	contentPath  = flag.String("content", "content/", "path to content files")
 	templatePath = flag.String("template", "template/", "path to template files")
